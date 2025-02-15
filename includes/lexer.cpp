9c51a495e269d7406b80b4761f20c2478b9df7c6
unknown <alexssmith124@protonmail.com>
2025-02-15 19:28:28 +1000

    added print and ints

--- includes/lexer.cpp
+++ includes/lexer.cpp
@@ -1,7 +1,9 @@
 #include "lexer.h"
 #include <regex>
+std::regex ints("(int)\\s([a-zA-Z_-][a-zA-Z0-9_]*)\\s(=)\\s*(\\d+)");
+std::regex prints("(print)\\s?\\((\")?([a-zA-Z_-])(\"?)\\)");
+
 
 std::string lex(const std::string& input) {
-	
     return input; // Dummy implementation
 }
--- includes/lexer.h
+++ includes/lexer.h
@@ -1,4 +1,9 @@
 #pragma once
 #include <string>
+#include <regex>
+
+std::regex ints;
+std::regex prints;
+
 
 std::string lex(const std::string& input); // Function declaration
